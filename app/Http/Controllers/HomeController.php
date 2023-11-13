ee93f7bae0b0c64fe0b38690226e0df4b5e7bb26
EscaleraJandel123 <134498020+EscaleraJandel123@users.noreply.github.com>
2023-11-13 18:49:15 +0800

    Applicant login register with homepage and admin dumps

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    // public function adminpage()
+    // {
+    //     return view("admin/index");
+    // }
+    // public function applicantPage()
+    // {
+    //     return view("applicant/index");
+    // }
+    public function index()
+    {
+        return view("/homepage/homepage");
+    }
+}
